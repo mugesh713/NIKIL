e511a430574742a41235cf3e627ea3fce09ceb2e
Mugesh V <125540370+mugesh713@users.noreply.github.com>
2025-01-31 09:02:13 +0530

    first

---
+++ src/assets/index.js
@@ -0,0 +1,101 @@
+import reactjs from './tech/reactjs.png'; // Only one import needed
+import typescript from './tech/typescript.png'; // Corrected path
+import tailwind from './tech/tailwind.png'; // Keep only this one import
+
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import shaq from './logo/npi.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import unity from './tech/unity.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import canva from './tech/canva.jpg'; // Assuming canva.jpg exists here
+
+import graphql from './tech/graphql.png';
+import blender from './tech/blender.png';
+import rubyrails from './tech/rubyrails.png';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+
+import komikult from './projects/komikult.png';
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  shaq,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  unity,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  blender,
+  rubyrails,
+  reactjs,
+  canva,
+  tailwind,
+  typescript,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+};
